cc99e3e71299458f8d90f6af08008d6945387413
Stas Sherstiuk <stas.sherstiuk@transwestern.com>
2024-05-27 12:48:13 +0300

    init

---
+++ src/app/propety.ts
@@ -0,0 +1,10 @@
+export class Property {
+
+    public constructor(name: string, image: string){
+        this.propertyName = name;
+        this.image = image;
+    }
+    // public id: string;
+    public propertyName: string;
+    public image: string;
+}
