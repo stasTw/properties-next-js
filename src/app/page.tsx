4a6169305f22f1977904dfb7d5d4d24e470680f9
Stas Sherstiuk <stas.sherstiuk@transwestern.com>
2024-05-30 13:56:54 +0300

    real api example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import properties from '../../public/GetProperties.json'
 import { ReactNode } from "react";
 
 export default function Home() {
-  getData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div className="grid grid-cols-12">
@@ -11,35 +10,8 @@ export default function Home() {
           <div>content</div>
         </div>
         <div className="col-span-4 list">
-          <GetList />
         </div>
       </div>
     </main>
   );
 }
-
-async function getData() {
-  console.log("Huyak");
-  console.log(properties);
-}
-
-function GetList() {
-  const props: Array<{name: string, address:string, imagePath:string}> = properties.map(prop => ({name:prop.BuildingName, address: prop.Address, imagePath: prop.MainImage?.ImagePath!}));
-  props.length = 20;
-  const markup: ReactNode = props.map(prop => {
-    return <li key={prop.name}>
-      <div className="properties-list__image">
-        <img src={prop.imagePath} alt="" />
-      </div>
-      <div className="content">
-        <div>{prop.name}</div>
-        <div>{prop.address}</div>
-      </div>
-    </li>
-  }
-  
-  )
-  return <ul className="properties-list">{markup}</ul>;
-}
-
-
---
+++ src/pages/posts.tsx
@@ -0,0 +1,91 @@
+import { ReactNode } from "react";
+import { useState } from 'react'
+import "../app/globals.css"
+export default function Posts(props:Props) {
+    const posts = props.posts;
+
+    const [searchItem, setSearchItem] = useState('')
+    const [filteredPosts, setFilteredPosts] = useState(posts);
+
+    const handleInputChange = (e: any) => {
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm);
+        
+        console.log(posts.length);
+        const filteredItems = posts.filter((post) =>
+            post.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+
+        setFilteredPosts(filteredItems)
+    }
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-12">
+        <div className="grid grid-cols-12">
+          <div className="col-span-8 map">
+            <input type="text" placeholder="Search" value={searchItem} onChange={handleInputChange}/>
+          </div>
+          <div className="col-span-4 list">
+            <GetList posts={filteredPosts} />
+          </div>
+        </div>
+      </main>
+            )
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+    console.log(res);
+    const posts: Post = await res.json()
+   
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
+
+  type ListProps = {
+    posts: Post[]
+  }
+
+  function GetList(listProps: ListProps) {
+    const posts = listProps.posts;
+    posts.length = 20;
+    const markup: ReactNode = posts.map(prop => {
+      return <li key={prop.id}>
+        <div className="properties-list__image">
+          <img src={prop.url} alt="" />
+        </div>
+        <div className="content">
+          <div>{prop.title}</div>
+          <div>{prop.title}</div>
+        </div>
+      </li>
+    })
+    return <ul className="properties-list">{markup}</ul>;
+  }
+
+
+  export interface Props {
+    posts: Array<Post>;
+  }
+  
+  export class Post {
+    public constructor(albumId:number, id:number, title: string, url:string,thumbnailUrl: string) {
+      this.albumId = albumId;
+      this.id = id;
+      this.title = title;
+      this.url = url;
+      this.thumbnailUrl = thumbnailUrl;
+    }
+    public albumId: number;
+    public id: number;
+    public title: string;
+    public url: string;
+    public thumbnailUrl: string;
+  }
