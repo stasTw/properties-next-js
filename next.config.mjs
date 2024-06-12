2eadf76c80c7aec6784129313624afdf13afef3f
Stas Sherstiuk <stas.sherstiuk@transwestern.com>
2024-06-12 20:28:52 +0300

    add redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     images: {
         domains: ['s3.amazonaws.com', 't4.ftcdn.net']
-      }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/properties',
+                permanent: true,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
